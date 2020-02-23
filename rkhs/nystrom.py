971b59046d9a9aef98601616d5ad500d47e323fc
Luis Montero <luismontero@hotmail.fr>
2020-02-24 00:13:14 +0100

    update on nystrom

--- rkhs/nystrom.py
+++ rkhs/nystrom.py
@@ -1,8 +1,8 @@
 import logging
-from typing import Callable
 
-from sklearn.metrics.pairwise import pairwise_kernels
 import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.metrics.pairwise import pairwise_kernels
 
 logger = logging.getLogger(__name__)
 
@@ -11,19 +11,45 @@ class PlainNystrom:
     def __init__(self, kernel: str = 'rbf', m: int = 100):
         self.m = m
         self.kernel = kernel
-        self.kernels = None
+        self.sample = None
 
-    def fit(self, X: np.ndarray, y: np.array = None, **kwargs):
-        c = X[:]
-        np.random.shuffle(c)
-        sample = c[:self.m]
-        self.kernels = lambda elt: pairwise_kernels(
-            elt, sample, metric=self.kernel, **kwargs)
+    def fit(self, X: np.ndarray, y: np.array = None):
+        self.sample = X[
+            np.random.choice(len(X), min(self.m, len(X)), replace=False)]
         return self
 
-    def transform(self, X: np.ndarray, y: np.array = None):
-        return self.kernels(X)
+    def transform(self, X: np.ndarray, y: np.array = None, **kwargs):
+        return pairwise_kernels(
+            X, self.sample, metric=self.kernel, **kwargs)
 
-    def fit_transform(self, X: np.ndarray, y: np.array = None):
+    def fit_transform(self, X: np.ndarray, y: np.array = None, **kwargs):
         self.fit(X, y)
-        return self.transform(X, y)
+        return self.transform(X, y, **kwargs)
+
+
+class PlainNystromRegressor(BaseEstimator):
+    def __init__(self, kernel: str = 'rbf', m: int = 100, lambda_reg: int = 0):
+        self.projector = PlainNystrom(kernel=kernel, m=m)
+        self.lambda_reg = lambda_reg
+        self.coeffs = None
+
+    def fit(self, X: np.ndarray, y: np.array = None, **kwargs):
+        k_nm = self.projector.fit_transform(X=X, y=y, **kwargs)
+        k_mm = self.projector.transform(X=self.projector.sample, y=y)
+        assert k_mm.shape[0] == k_mm.shape[1] == k_nm.shape[1]
+        n = len(X)
+        pseudo_inv = np.linalg.pinv(
+            (k_nm.T @ k_nm) + (self.lambda_reg * n * k_mm))
+        self.coeffs = pseudo_inv @ k_nm.T @ y
+        return self
+
+    def predict(self, X):
+        projection = self.projector.transform(X=X)
+        return projection @ self.coeffs
+
+
+class ALSNystrom:
+    # TODO: Implement this using; https://github.com/LCSL/bless
+    def __init__(self):
+        raise NotImplementedError(
+            "Must find scalable way to compute leverage scores")
